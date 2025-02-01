d45a95b9f505382cb8368190325513e7b03a672a
Vaishnavi Pawar <vaishnavi.22211188@viit.ac.in>
2025-02-01 15:34:25 +0530

    all setup configured

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer"
+
+// we are using disk storage, we can also use memory storage too.
+// in function file parameter is for uploading the file through multer because all other data is configured but file is not so we use multer or express file upload.
+// cb is callback
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage})
